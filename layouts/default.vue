e938d5d478b0f69394b3e1f62f15003a2ce5ecd9
Hamarko <hamarkoav@gmail.com>
2020-12-04 17:35:34 +0200

    Chat layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,17 +11,8 @@
 
 <style>
 html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
+  font-family:"Open Sans";    
+  font-size: 14px;  
   -ms-text-size-adjust: 100%;
   -webkit-text-size-adjust: 100%;
   -moz-osx-font-smoothing: grayscale;
--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,146 @@
 <template>   
     <b-row class="content" >
       <b-col cols="9" class="chat">
-      <h1>Work</h1>
+        <b-row class='chat-header'>         
+          <img src="../static/gif2.jpg" alt="">         
+          <b-col class="header-text">
+            <h2>Reverse bot</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, 
+               sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+               Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+               nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+               reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+               pariatur. </p>
+          </b-col>        
+        </b-row>
+        <div class="chat-area">
+
+        </div>
+        <div class="chat-form">
+
+        </div>
       </b-col>
       <b-col cols="3" class="users">
-        <b-row>
+        <b-row style="margin-bottom:10px">
           <b-col class="card-activ"><p>Onlin</p></b-col>
           <b-col class="card"><p>All</p></b-col>
         </b-row>
-        <b-row>
-          
+        <b-row class="card-container">
+          <div class="user-cart">
+            <div class="img-wrapper">
+              <div class="img-box">
+                <img src="../static/gif1.jpg" alt="">
+              </div>
+              <svg class='circle-svg' viewBox="0 0 80 80" width="20" height="20">
+                <circle class="circle" cx="40" cy="40" r="38"/>
+              </svg>              
+            </div>
+            <div class="text-content">
+              <h6>Echo bot</h6>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+            </div>            
+          </div>          
+          <div class="user-cart">
+            <div class="img-wrapper">
+              <div class="img-box">
+                <img src="../static/gif1.jpg" alt="">
+              </div>
+              <svg class='circle-svg' viewBox="0 0 80 80" width="20" height="20">
+                <circle class="circle" cx="40" cy="40" r="38"/>
+              </svg>              
+            </div>
+            <div class="text-content">
+              <h6>Echo bot</h6>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+            </div>            
+          </div>          
         </b-row>
+        <div class="form-shearch">
+          <b-form-input placeholder="Search..." ></b-form-input>
+        </div>        
       </b-col>      
     </b-row>
    
 </template>
 
 <script>
-export default {}
+import Message from "@/components/Message";
+export default {
+  data:()=> ({   
+    messages:[],
+  }),
+}
 </script>
 
 <style>
 .content{
-  min-height: 651px;;
+  min-height: 651px;
+}
+
+.chat-header{
+  height: 170px;
+  background-color: #becbd9;
+}
+.chat-header img{
+  height: 170px;
+  width: 170px;
+}
+.chat-form {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  padding: 1rem;
+  height: 80px;
+  background: #212121;
+}
+.chat-area {
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 80px;
+  padding: 1rem;
+  overflow-y: auto;
+}
+.card-container{
+  min-height: 540px;
+  justify-content: flex-start;
+  align-content: flex-start;
+}
+.user-cart{  
+  display: flex;    
+  height: 70px;
+  padding: 5px;
+  }
+.text-content{    
+  flex-direction: column;
+  overflow: hidden;
+  margin-left: 16px;
+}
+.text-content p{   
+  overflow: hidden;
+  text-overflow: ellipsis; 
+  color: #777777;
+}
+.text-content h6{ 
+  margin-bottom:0;
+}
+.form-shearch{
+  justify-self: flex-end;
+}
+.user-cart img{
+  width: 60px;
+  height: 60px;
+  border-radius: 3px;
+  background-color: #ededed;
+}
+.circle-svg{
+    margin-left: 45px;
+    margin-top: -30px;
+}
+.circle {  
+  fill: #1fd63d;   
 }
 .users{  
   background-color:#fff;
@@ -31,8 +149,7 @@ export default {}
 }
 .chat{
   background-color: #d7dfe7ff;
-  border-radius: 4px;
-  
+  border-radius: 4px; 
 }
 .card-activ{
   height: 42px;}
